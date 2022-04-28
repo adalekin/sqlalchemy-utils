5209366efff386aca023084877512f267f7954ee
Aleksey Dalekin <adalekin@gmail.com>
2022-04-29 01:03:53 +0300

    Use UtcDateTime instead of DateTime

--- sqlalchemy_utils/mixins.py
+++ sqlalchemy_utils/mixins.py
@@ -1,6 +1,7 @@
 import datetime
 
-from sqlalchemy import Column, DateTime, Integer
+from sqlalchemy import Column, Integer
+from sqlalchemy_utc import UtcDateTime
 
 
 class MixinWithID:
@@ -12,9 +13,9 @@ class MixinWithID:
 
 class MixinWithAutoNow:
     updated_at = Column(
-        DateTime(),
+        UtcDateTime(),
         nullable=False,
         default=lambda: datetime.datetime.utcnow(),
         onupdate=lambda: datetime.datetime.utcnow(),
     )
-    created_at = Column(DateTime(), nullable=False, default=lambda: datetime.datetime.utcnow())
+    created_at = Column(UtcDateTime(), nullable=False, default=lambda: datetime.datetime.utcnow())
